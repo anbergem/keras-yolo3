9c482fbbeb19a5cf07ae21a348ac2fbf29b85c44
Jørn Bersvendsen <jornb87@gmail.com>
2019-02-04 20:30:22 +0100

    Improved parsing of voc annotation files

--- voc.py
+++ voc.py
@@ -2,66 +2,86 @@ import numpy as np
 import os
 import xml.etree.ElementTree as ET
 import pickle
+from collections import defaultdict
 
-def parse_voc_annotation(ann_dir, img_dir, cache_name, labels=[]):
+def _parse_voc_object(object_node: ET.Element):
+    return {
+        "name": object_node.findtext("name"),
+        "xmin": int(object_node.findtext("bndbox/xmin")),
+        "xmax": int(object_node.findtext("bndbox/xmax")),
+        "ymin": int(object_node.findtext("bndbox/ymin")),
+        "ymax": int(object_node.findtext("bndbox/ymax")),
+    }
+
+def _parse_voc_annotation(annotation_node: ET.Element, image_directory, labels):
+    instance = {
+        "filename": os.path.join(image_directory, annotation_node.findtext("filename")),
+        "width": int(annotation_node.findtext("size/width")),
+        "height": int(annotation_node.findtext("size/height")),
+        "object": [_parse_voc_object(node) for node in annotation_node.findall("object")]
+    }
+
+    # Filter objects by label
+    if labels:
+        instance["object"] = [obj for obj in instance["object"] if obj["name"] in labels]
+
+    return instance
+
+def parse_voc_annotation_file(filename, image_directory, labels=None):
+    tree = ET.parse(filename)
+
+    if tree.getroot() == "annotation":
+        # Single file with single annotation
+        inst = [_parse_voc_annotation(tree, image_directory, labels)]
+        instances = [inst]
+    else:
+        # File with multiple annotations
+        instances = [_parse_voc_annotation(node, image_directory, labels) for node in tree.findall("annotation")]
+    
+    # Filter instances without objects
+    instances = [inst for inst in instances if inst["object"]]
+
+    # Count labels
+    label_counts = defaultdict(int)
+    for inst in instances:
+        for obj in inst["object"]:
+            label_counts[obj["name"]] += 1
+
+    return instances, label_counts
+
+
+def parse_voc_annotation(ann_dir, image_directory, cache_name, labels=None):
     if os.path.exists(cache_name):
-        with open(cache_name, 'rb') as handle:
+        with open(cache_name, "rb") as handle:
             cache = pickle.load(handle)
-        all_insts, seen_labels = cache['all_insts'], cache['seen_labels']
+        all_insts, label_counts = cache["all_insts"], cache["label_counts"]
     else:
         all_insts = []
-        seen_labels = {}
+        label_counts = defaultdict(int)
         
         for ann in sorted(os.listdir(ann_dir)):
-            img = {'object':[]}
+            filename = os.path.join(ann_dir, ann)
+
+            # Skip folders etc.
+            if not os.path.isfile(filename):
+                continue
 
             try:
-                tree = ET.parse(ann_dir + ann)
+                file_instances, file_label_counts = parse_voc_annotation_file(filename, image_directory, labels)
             except Exception as e:
-                print(e)
-                print('Ignore this bad annotation: ' + ann_dir + ann)
+                print("Failed to parse annotation file %s: %s" % (filename, e))
                 continue
-            
-            for elem in tree.iter():
-                if 'filename' in elem.tag:
-                    img['filename'] = img_dir + elem.text
-                if 'width' in elem.tag:
-                    img['width'] = int(elem.text)
-                if 'height' in elem.tag:
-                    img['height'] = int(elem.text)
-                if 'object' in elem.tag or 'part' in elem.tag:
-                    obj = {}
-                    
-                    for attr in list(elem):
-                        if 'name' in attr.tag:
-                            obj['name'] = attr.text
 
-                            if obj['name'] in seen_labels:
-                                seen_labels[obj['name']] += 1
-                            else:
-                                seen_labels[obj['name']] = 1
-                            
-                            if len(labels) > 0 and obj['name'] not in labels:
-                                break
-                            else:
-                                img['object'] += [obj]
-                                
-                        if 'bndbox' in attr.tag:
-                            for dim in list(attr):
-                                if 'xmin' in dim.tag:
-                                    obj['xmin'] = int(round(float(dim.text)))
-                                if 'ymin' in dim.tag:
-                                    obj['ymin'] = int(round(float(dim.text)))
-                                if 'xmax' in dim.tag:
-                                    obj['xmax'] = int(round(float(dim.text)))
-                                if 'ymax' in dim.tag:
-                                    obj['ymax'] = int(round(float(dim.text)))
+            # Add together instances and label counts
+            all_insts += file_instances
+            for k, v in file_label_counts.items():
+                label_counts[k] += v
 
-            if len(img['object']) > 0:
-                all_insts += [img]
-
-        cache = {'all_insts': all_insts, 'seen_labels': seen_labels}
-        with open(cache_name, 'wb') as handle:
-            pickle.dump(cache, handle, protocol=pickle.HIGHEST_PROTOCOL)    
+        # Save cache
+        if cache_name:
+            os.makedirs(os.path.dirname(cache_name), exist_ok=True)
+            with open(cache_name, "wb") as handle:
+                cache = {"all_insts": all_insts, "label_counts": label_counts}
+                pickle.dump(cache, handle, protocol=pickle.HIGHEST_PROTOCOL)
                         
-    return all_insts, seen_labels
+    return all_insts, label_counts
